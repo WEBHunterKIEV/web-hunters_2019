30f83ac4a0747a09327046520837267dc58e2e49
Hunter1 <info@web-hunters.com>
2019-09-20 09:57:50 +0300

    menu edit

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+//const autoprefixer = require('gulp-autoprefixer');
 
 
 
@@ -28,12 +29,23 @@ gulp.task('compile:pug', function buildHTML() {
 });
 
 
-// SASS
+// SASS 
 gulp.task('compile:sass', function () {
     return gulp.src('dev/styles/main.scss')
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(gulp.dest('distr/css'));
 });
+
+// autoprefixer 
+/*
+gulp.task('autoprefixer', function () {
+    return gulp.src('distr/css/main.css')
+    .pipe(autoprefixer({
+        browsers: ['last 16 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('distr/css/autoprefix/'))
+});*/
 
 // WATCHERS
 gulp.task('watch', function() {
